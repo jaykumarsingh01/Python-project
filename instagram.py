57768352d1f40515c337a16643819a13be459aa8
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-04 08:40:59 +0530

    new file

---
+++ instagram.py
@@ -0,0 +1,26 @@
+# How to make a Simplest Instagram Bot in Python
+
+# pip install instabot
+
+from instabot import Bot
+bot=Bot()
+bot.login(username="your username",passwords="your passwords")
+bot.follow('virat.kohli')
+bot.upload_photo("C:/Users/Dell/OneDrive/Pictures/jay.jpg",caption="wow___")
+
+# if you want to unfollow any person then: 
+bot.unfollow("virat.kohli")
+
+# if you want to send message for multiple person in instagram
+bot.send_message("hey how are you",["virat.kohli", "jaykumarsingh_01"])
+
+# how to know your followers and following on the instagram:
+followers =bot.get_user_followers("your user id")
+for followers in followers:
+    print(bot.get_user_info(follower))
+
+
+following=bot.get_user_following("your user id")
+for Following in following:
+    print(bot.get_user_info(Following))
+
