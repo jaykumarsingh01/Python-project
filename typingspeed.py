38115363ecd51a56dcc2620b06fc787e09224e66
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-07 09:46:29 +0530

    file

---
+++ typingspeed.py
@@ -0,0 +1,16 @@
+from time import *
+import random as r
+
+
+test =[
+    "A paragraph is a series of sentences that are organized and coherent,"
+" and are all related to a single topic. "
+"Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. "
+"This is because paragraphs show a reader where the subdivisions of an essay begin and end,"
+" and thus help the reader see the organization of the essay and grasp its main points.", 
+"my name is jay kumar singh",
+"welcome to our family"
+]
+test1 =r.choice(test)
+print("---------typing speed -----------")
+print(test1)
