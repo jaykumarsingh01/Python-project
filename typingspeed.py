6165df3f1bab1c244e06fbd432f261990dac3250
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-07 09:55:02 +0530

    updated file

--- typingspeed.py
+++ typingspeed.py
@@ -2,15 +2,36 @@ from time import *
 import random as r
 
 
-test =[
-    "A paragraph is a series of sentences that are organized and coherent,"
-" and are all related to a single topic. "
-"Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. "
-"This is because paragraphs show a reader where the subdivisions of an essay begin and end,"
-" and thus help the reader see the organization of the essay and grasp its main points.", 
-"my name is jay kumar singh",
-"welcome to our family"
-]
-test1 =r.choice(test)
-print("---------typing speed -----------")
-print(test1)
+print(time())
+
+# def mistake(partest,usertest):
+#   error = 0
+#   for i in range (len(partest)):
+#     try:
+#       if partest[i] != usertest[i]:
+#         error = error + 1
+      
+#     except:
+#         error = error + 1
+#   return error
+         
+       
+
+ 
+# test =[
+#     "A paragraph is a series of sentences that are organized and coherent,"
+# " and are all related to a single topic. "
+# "Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. "
+# "This is because paragraphs show a reader where the subdivisions of an essay begin and end,"
+# " and thus help the reader see the organization of the essay and grasp its main points.", 
+# "my name is jay kumar singh",
+# "welcome to our family"
+# ]
+# test1 =r.choice(test)
+# print("---------typing speed -----------")
+# print(test1)
+
+# print()
+# print()
+# testinput=input("Enter: ")
+
