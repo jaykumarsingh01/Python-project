8bd5c7d7aa4a5e6bfa408e86cdf225549a0e57d1
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-08 21:48:52 +0530

    updated file

--- typingspeed.py
+++ typingspeed.py
@@ -1,50 +1,48 @@
 from time import *
 import random as r
 
+def mistake(partest, usertest):
+    error = 0
+    for i in range(len(partest)):
+        try:
+            if partest[i] != usertest[i]:
+                error += 1
+        except:
+            error += 1
+    return error
 
-# print(time())
+def speed_time(time_start, time_end, userinput):
+    time_delay = time_end - time_start
+    time_R = round(time_delay, 2)
+    speed = len(userinput) / time_R
+    return round(speed)
 
-def mistake(partest,usertest):
-  error = 0
-  for i in range (len(partest)):
-    try:
-      if partest[i] != usertest[i]:
-        error = error + 1
-      
-    except:
-        error = error + 1
-  return error
+while True:
+    ck = input("Ready to test: yes / no: ").lower()
+
+    if ck == "yes":
+        test = [
+            
+            "My Name is Jay Kumar Singh"
          
-def speed_time (time_start, time_end, userinput):
- time_delay = time_end-time_start
- time_R =round(time_delay,2)
- speed = len(userinput)/ time_R
- return round(speed) 
-       
+        ]
+        test1 = r.choice(test)
+        print("--------- Typing Speed Test -----------")
+        print(test1)
+        print()
+        print()
+        time_1 = time()
+        testinput = input("Enter: ")
+        time_2 = time()
 
- 
- test =[
-    "A paragraph is a series of sentences that are organized and coherent,"
-" and are all related to a single topic. "
-"Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. "
-"This is because paragraphs show a reader where the subdivisions of an essay begin and end,"
-" and thus help the reader see the organization of the essay and grasp its main points.", 
-"my name is jay kumar singh",
-"welcome to our family"
-]
-test1 =r.choice(test)
-print("---------typing speed -----------")
-print(test1)
+        print('Speed:', speed_time(time_1, time_2, testinput), "w/sec")
+        print('Error:', mistake(test1, testinput))
 
-print()
-print()
-time_1 =time()
-testinput=input("Enter: ")
-time_2 = time()
+    elif ck == "no":
+        print("Thank you!")
+        break
 
+    else:
+        print("Wrong input, please type 'yes' or 'no'.")
 
-print('Speed : ',speed_time (time_1, time_2, testinput), "w/sec")
-print('Error : ',  mistake(test1,testinput))
-
-
-
+        # Jay Kumar Singh
