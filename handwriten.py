4a90ace19d57f4daa7ffb976fdffb2d257546907
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-10 07:58:22 +0530

    updated file

---
+++ handwriten.py
@@ -0,0 +1,9 @@
+import pywhatkit as pw
+
+
+txt="""In today's digital era, student 
+management systems play a crucial role in maintaining and organizing academic records, attendance, and personal details 
+"""
+
+pw.text_to_handwriting(txt,"demo1.png",[0,0,138])
+print("THANKYOU") 
