9f60f4443811908758e4af735b1900bba0d7ad31
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-11 10:57:19 +0530

    updated file

--- digitalclock.py
+++ digitalclock.py
@@ -6,9 +6,10 @@ clock.geometry('1000x500')
 clock.config(bg='Wheat')
 
 
-# lab_hr=Label(clock,text="00",font=('Times New Roman',60,"bold"),
-#              bg='black',fg="white")
+lab_hr=Label(clock,text="00",font=('Times New Roman',60,"bold"),
+             bg='black',fg="white")
 
+lab_hr.place(x=40,y=40,height=110,width=100)
 
 
 
