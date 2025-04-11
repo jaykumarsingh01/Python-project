4b162983668b1b614b1487821a3f581d0ab063db
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-11 11:19:48 +0530

    updated file

--- digitalclock.py
+++ digitalclock.py
@@ -9,25 +9,37 @@ clock.config(bg='Wheat')
 lab_hr=Label(clock,text="00",font=('Times New Roman',60,"bold"),
              bg='black',fg="white")
 
-lab_hr.place(x=120,y=40,height=110,width=100)
+lab_hr.place(x=120,y=50,height=110,width=100)
+lab_hr_txt=Label(clock,text="Hour",font=('Times New Roman',20,"bold"),
+             bg='black',fg="white")
+lab_hr_txt.place(x=120,y=190,height=40,width=100)
 
 
 lab_min=Label(clock,text="00",font=('Times New Roman',60,"bold"),
              bg='black',fg="white")
 
-lab_min.place(x=340,y=40,height=110,width=100)
+lab_min.place(x=340,y=50,height=110,width=100)
+lab_min_txt=Label(clock,text="Min.",font=('Times New Roman',20,"bold"),
+             bg='black',fg="white")
+lab_min_txt.place(x=340,y=190,height=40,width=100)
 
 
 lab_sec=Label(clock,text="00",font=('Times New Roman',60,"bold"),
              bg='black',fg="white")
 
-lab_sec.place(x=560,y=40,height=110,width=100)
+lab_sec.place(x=560,y=50,height=110,width=100)
+lab_sec_txt=Label(clock,text="Sec.",font=('Times New Roman',20,"bold"),
+             bg='black',fg="white")
+lab_sec_txt.place(x=560,y=190,height=40,width=100)
 
 
 lab_am=Label(clock,text="00",font=('Times New Roman',60,"bold"),
              bg='black',fg="white")
 
-lab_am.place(x=780,y=40,height=110,width=100)
+lab_am.place(x=780,y=50,height=110,width=100)
+lab_am_txt=Label(clock,text="AM/PM",font=('Times New Roman',20,"bold"),
+             bg='black',fg="white")
+lab_am_txt.place(x=780,y=190,height=40,width=100)
 
 
 
