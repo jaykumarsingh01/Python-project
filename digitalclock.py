89e77fca87ef4b9956ab72c5c83c029e1db41f14
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-11 10:52:30 +0530

    updated file

---
+++ digitalclock.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+clock=Tk()
+clock.title('Digital Clock')
+clock.geometry('1000x500')
+clock.config(bg='Wheat')
+
+
+# lab_hr=Label(clock,text="00",font=('Times New Roman',60,"bold"),
+#              bg='black',fg="white")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clock.mainloop()
