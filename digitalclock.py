a9358d35fbd82acf359a0731cbe57e723fb1cb2f
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-11 11:44:02 +0530

    updated file

--- digitalclock.py
+++ digitalclock.py
@@ -6,11 +6,23 @@ import datetime
 def date_time():
     time =datetime.datetime.now()
     hr = time.strftime('%I')
+    min = time.strftime('%M')
+    sec = time.strftime('%S')
+    am = time.strftime('%p ')
+    lab_hr.config(text=hr)
+    lab_min.config(text=min)
+    lab_sec.config(text=sec)
+    lab_am.config(text=am)
+    lab_hr.after(200,date_time)
+
 
 clock=Tk()
 clock.title('Digital Clock')
 clock.geometry('1000x500')
 clock.config(bg='Wheat')
+
+
+#****************Time***********
 
 
 lab_hr=Label(clock,text="00",font=('Times New Roman',60,"bold"),
@@ -40,17 +52,40 @@ lab_sec_txt=Label(clock,text="Sec.",font=('Times New Roman',20,"bold"),
 lab_sec_txt.place(x=560,y=190,height=40,width=100)
 
 
-lab_am=Label(clock,text="00",font=('Times New Roman',60,"bold"),
+lab_am=Label(clock,text="00",font=('Times New Roman',40,"bold"),
              bg='black',fg="white")
 
 lab_am.place(x=780,y=50,height=110,width=100)
-lab_am_txt=Label(clock,text="AM/PM",font=('Times New Roman',20,"bold"),
+lab_am_txt=Label(clock,text="Am/Pm",font=('Times New Roman',20,"bold"),
              bg='black',fg="white")
 lab_am_txt.place(x=780,y=190,height=40,width=100)
 
 
 
 
+# Date
+
+
+
+
+
+lab_date=Label(clock,text="00",font=('Times New Roman',60,"bold"),
+             bg='black',fg="white")
+
+lab_date.place(x=120,y=270,height=110,width=100)
+lab_date_txt=Label(clock,text="Date",font=('Times New Roman',20,"bold"),
+             bg='black',fg="white")
+lab_date_txt.place(x=120,y=410,height=40,width=100)
+
+
+
+lab_mon=Label(clock,text="00",font=('Times New Roman',60,"bold"),
+             bg='black',fg="white")
+
+lab_mon.place(x=340,y=270,height=110,width=100)
+lab_mon_txt=Label(clock,text="Month",font=('Times New Roman',20,"bold"),
+             bg='black',fg="white")
+lab_mon_txt.place(x=340,y=410,height=40,width=100)
 
 
 
@@ -68,6 +103,22 @@ lab_am_txt.place(x=780,y=190,height=40,width=100)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+date_time()
 
 
 
