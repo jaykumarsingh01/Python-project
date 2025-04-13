9b22597ba543d70832a852815afa5a26a7d8d500
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-13 14:00:23 +0530

    Update Skype

--- Skype.py
+++ Skype.py
@@ -19,3 +19,4 @@ for i in contact:
 
 
                                                                          
+                                                                        # Jay Kumar Singh
