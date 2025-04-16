3b855e874dc7d1a52cf3874784a6e0ad30cc93cd
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-16 16:49:32 +0530

    updated file

---
+++ internetspeed.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import speedtest
+
+sp =Tk()
+sp.title("speed test")
+sp.geometry("500x500")
+sp.config(bg="pink")
+
+lab= Label(sp,text="speed test",font=("Time New Roman",40,"bold"))
+lab.place(x=40,y=40,)
+
+
+
+
+
+sp.mainloop()
