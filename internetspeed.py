d3869bdc8ae8fa10282e16bab55d90552fd8d52e
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-16 17:35:59 +0530

    updated file

--- internetspeed.py
+++ internetspeed.py
@@ -6,11 +6,19 @@ sp.title("speed test")
 sp.geometry("500x500")
 sp.config(bg="pink")
 
-lab= Label(sp,text="speed test",font=("Time New Roman",40,"bold"))
-lab.place(x=40,y=40,)
+lab= Label(sp,text="Speed test",font=("Time New Roman",40,"bold"),bg="pink",fg="black")
+lab.place(x=70,y=40,height=50,width=380)
 
+lab= Label(sp,text="Download speed",font=("Time New Roman",40,"bold"),bg="pink",fg="black")
+lab.place(x=70,y=130,height=50,width=410)
 
+lab= Label(sp,text="00",font=("Time New Roman",40,"bold"),bg="pink",fg="black")
+lab.place(x=70,y=200,height=50,width=380)
 
+lab= Label(sp,text="Upload speed",font=("Time New Roman",40,"bold"),bg="pink",fg="black")
+lab.place(x=70,y=290,height=50,width=380)
 
+lab= Label(sp,text="00",font=("Time New Roman",40,"bold"),bg="pink",fg="black")
+lab.place(x=70,y=360,height=50,width=380)
 
 sp.mainloop()
