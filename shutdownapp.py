2fa62870281b43bf1160e4e528fab85231cf5f12
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-18 12:13:00 +0530

    updated file

--- shutdownapp.py
+++ shutdownapp.py
@@ -13,7 +13,8 @@ rt_buttom.place(x=150,y=170,height=50,width=200)
 lg_buttom  = Button(st,text="Log-Out",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
 lg_buttom.place(x=150,y=270,height=50,width=200)
 
-
+st_buttom  = Button(st,text="Log-Out",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
+st_buttom.place(x=150,y=370,height=50,width=200)
 
 
 
