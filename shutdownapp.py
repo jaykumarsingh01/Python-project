cf93d8a186040b85cf69eeda944d7df93ac4118b
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-18 12:05:47 +0530

    updated file

--- shutdownapp.py
+++ shutdownapp.py
@@ -4,6 +4,8 @@ st=Tk()
 st.title("Shutdown App")
 st.geometry("500x500")
 st.config(bg="DeepSkyBlue")
+r_buttom  = Button(st,text="restart",font= ("time new roman",30,"bold"),relief=RAISED,cursor="plus")
+r_buttom.place(x=200,y=60,height=50,width=100)
 
 
 
