9d7a23cebfd605e838b618a17d58274c9b9d8610
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-18 12:20:48 +0530

    updated file

--- shutdownapp.py
+++ shutdownapp.py
@@ -1,10 +1,27 @@
 from tkinter import *
 import os
+def Restart():
+    os.system("shutdown /r /t 1")
+
+
+def Restart_time():
+     os.system("shutdown /r /t 20")
+
+def logout():
+     os.system("shutdown -l")
+
+def shutdown():
+     os.system("shutdown /s /t 1")
+
+
 st=Tk()
 st.title("Shutdown App")
 st.geometry("500x500")
 st.config(bg="DeepSkyBlue")
-r_buttom  = Button(st,text="Restart",font= ("time new roman",30,"bold"),relief=RAISED,cursor="plus")
+
+
+
+r_buttom  = Button(st,text="Restart",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
 r_buttom.place(x=150,y=60,height=50,width=200)
 
 rt_buttom  = Button(st,text="Restart time",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
@@ -13,19 +30,8 @@ rt_buttom.place(x=150,y=170,height=50,width=200)
 lg_buttom  = Button(st,text="Log-Out",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
 lg_buttom.place(x=150,y=270,height=50,width=200)
 
-st_buttom  = Button(st,text="Log-Out",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
+st_buttom  = Button(st,text="Shutdown",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
 st_buttom.place(x=150,y=370,height=50,width=200)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
