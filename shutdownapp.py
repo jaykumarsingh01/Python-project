905c52a56cb24ab5dfaaad68efd99b836db98be2
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-18 12:10:25 +0530

    updated file

--- shutdownapp.py
+++ shutdownapp.py
@@ -4,10 +4,14 @@ st=Tk()
 st.title("Shutdown App")
 st.geometry("500x500")
 st.config(bg="DeepSkyBlue")
-r_buttom  = Button(st,text="restart",font= ("time new roman",30,"bold"),relief=RAISED,cursor="plus")
-r_buttom.place(x=200,y=60,height=50,width=100)
+r_buttom  = Button(st,text="Restart",font= ("time new roman",30,"bold"),relief=RAISED,cursor="plus")
+r_buttom.place(x=150,y=60,height=50,width=200)
 
+rt_buttom  = Button(st,text="Restart time",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
+rt_buttom.place(x=150,y=170,height=50,width=200)
 
+lg_buttom  = Button(st,text="Log-Out",font= ("time new roman",20,"bold"),relief=RAISED,cursor="plus")
+lg_buttom.place(x=150,y=270,height=50,width=200)
 
 
 
