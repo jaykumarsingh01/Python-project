09f959ec92c8509a8bc543660b2f7eea6c9469a0
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-23 12:19:20 +0530

    updated file

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,66 @@
 import pyautogui
+from tkinter import *
+
+# ss=pyautogui.screenshot()
+# ss.save("test1.png")
+
+win=Tk()
+win.title("jay ss")
+win.geometry("400x300")
+win.config(bg="pink")
+win.resizable(False,False)
 
 
-ss=pyautogui.screenshot()
-ss.save("test1.png")
+button=Button(win,text="Done",font=('times new roman',50))
+button.place(x=100,y=100,height=100,width=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+win.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
