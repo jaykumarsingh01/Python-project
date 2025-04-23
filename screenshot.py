f5ea1b79f9842813e920e3d4a8070a82a06ab0e0
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-23 12:34:12 +0530

    updated file

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,11 @@
 import pyautogui
 from tkinter import *
 def take_ss():
+        # print(entry.get())
+        add_data=entry.get()
+        path= add_data+"\\test.png"
+        print(path)
+
         ss=pyautogui.screenshot()
         ss.save("test1.png")
 
@@ -21,7 +26,7 @@ entry.place(x=10,height=70,width=660,y=50)
 
 
 
-button=Button(win,text="Done",font=('times new roman',50))
+button=Button(win,text="Done",font=('times new roman',50),command=take_ss)
 button.place(x=250,y=140,height=100,width=200)
 
 
